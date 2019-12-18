e0a311fcca85de4aeab16b19c23bb5a4ff911b43
Ryan Lesson <ryan.lesson@tekzenit.com>
2019-12-18 15:22:31 -0600

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'star-angular';
+  title = "star-angular";
+  stars = [1, 2, 3, 4, 5];
+  rating = 0;
+  hoverState = 0;
+
+  enter(i) {
+    this.hoverState = i;
+  }
+
+  leave() {
+    this.hoverState = 0;
+  }
+
+  updateRating(i) {
+    this.rating = i;
+  }
 }
